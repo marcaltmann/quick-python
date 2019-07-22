bb37e8716345297021a24d357c46cfac7b6aec95
Marc Altmann <marcaltmann@posteo.de>
2019-07-22 16:57:47 +0200

    Add list example

--- sort.py
+++ sort.py
@@ -12,3 +12,12 @@ print(word_list)
 
 word_list.sort(key=compare_num_of_chars)
 print(word_list)
+
+def compare_lists(sublist):
+    return sublist[1]
+
+list_list = [[1, 2, 3], [2, 1, 3], [4, 0, 1]]
+
+list_list.sort(key=compare_lists)
+
+print(list_list)
